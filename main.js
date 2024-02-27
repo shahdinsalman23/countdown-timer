9e40093e47109080c540cec7d67d7a187dfc6761
Shahdin Salman <shahdinsalman@gmail.com>
2024-02-27 16:07:03 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+import { differenceInSeconds } from "date-fns";
+import inquirer from "inquirer";
+const res = await inquirer.prompt({
+    type: "number",
+    name: "userInput",
+    message: "Please enter the amount of second :",
+    validate: (input) => {
+        if (isNaN(input)) {
+            return "Please enter valid number";
+        }
+        else if (input > 60) {
+            return "Second must be in 60";
+        }
+        else {
+            return true;
+        }
+    }
+});
+function timer(val) {
+    const initialTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(initialTime);
+    setInterval(() => {
+        const currentTime = new Date();
+        const timeDiff = differenceInSeconds(intervalTime, currentTime);
+        if (timeDiff <= 0) {
+            console.log("Timer has expired");
+            process.exit();
+        }
+        const minute = Math.floor((timeDiff % (3600 * 24)) / 3600);
+        const second = Math.floor(timeDiff % 60);
+        console.log(`${minute.toString().padStart(2, "0")} : ${second.toString().padStart(2, "0")}`);
+    }, 1000);
+}
+timer(res.userInput);
